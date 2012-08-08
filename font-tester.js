c25847a8bedb2e904e475c912f3afd42d1c6db18
Ufuk Kayserilioglu <ufuk@enkuba.com>
2012-08-08 22:05:43 +0300

    Improvements...

--- font-tester.js
+++ font-tester.js
@@ -1,28 +1,81 @@
-(function($){
-  var html= '<div id="toolbar" style="border-bottom: 1px solid #aaa; position: fixed; top: 0; width: 100%; height: 25px; z-index: 10000"><select id="font"></select></div>';
-  $("body").append(html);
+(function (d) {
+})(document);
+(function(d){
+  function load(src, callback){
+    var s = d.createElement('script'),
+        onEvent = ('onreadystatechange' in s) ? 'onreadystatechange' : 'onload';
 
-  url = "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyByunO2GTypdbmc9izIFPERHIxHSWhh4Sw&callback=?"
+    s[onEvent] = function () {
+        if (("loaded,complete").indexOf(this.readyState || "loaded") > -1) {
+            s[onEvent] = null;
 
-  $.getJSON(url, function(data) {
-    $("select#font").html(
+            // Call your code here
+            if (callback) {
+              callback(jQuery);
+            }
+        }
+    };
+
+    // Set the src to the combo script url, e.g.
+    s.src = src
+    d.getElementsByTagName('head')[0].appendChild(s);
+  }
+
+  var FONT_URL_PREFIX = 'http://fonts.googleapis.com/css';
+  var API_PATH = "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyByunO2GTypdbmc9izIFPERHIxHSWhh4Sw&callback=?"
+
+  function buildFontUrl(family, variants, subsets) {
+    return FONT_URL_PREFIX + "?family=" + family + ":" + variants + "&subset=" + subsets;
+  }
+
+  function removeCurrentLink(head) {
+    $("link[rel='stylesheet'][href^='" + FONT_URL_PREFIX + "']", $head).remove();
+  }
+
+  function loadFonts($selector) {
+    $selector.html(
       $.map(data.items, function(item) {
-        console.log (item.subsets);
-        if ($.inArray('latin-ext', item.subsets))
-          return '<option value="' + item.family + '">' + item.family + '</option>';
+        if ($.inArray('latin-ext', item.subsets) != -1)
+          return '<option value="' + item.variants.join(',') + '" data-subsets="' + item.subsets.join(',') + '">' + item.family + '</option>';
         else
           return null;
       }).join('')
-    ).change(function (e) {
-      var family = $("select#font").val();
-      var $head = $("head");
-      $("link[rel='stylesheet'][href^='http://fonts.googleapis.com/css']", $head).remove();
-      $head.append($("<link/>", 
-          { rel: "stylesheet", 
-            href: "http://fonts.googleapis.com/css?family=" + family + "&subset=latin,latin-ext", 
-            type: "text/css" }));
-      $head.append("<style> body { font-family: '" + family + "' !important }");
-      //$('body').css("font-family", "'" + family + "' !important");
-    });
-  })
-})(jQuery)
+    );    
+  }
+
+  function (e) {
+    var $selected = $(this).children(':selected');
+    var variants = $selected.val();
+    var family = $selected.text();
+    var subsets = $selected.attr('data-subsets');
+
+    var href = buildFontUrl(family, variants, subsets);
+
+    removeCurrentLink($head);
+    $head.append($("<link/>", 
+        { rel: "stylesheet", 
+          href: href, 
+          type: "text/css" }));
+    $head.append("<style> body { font-family: '" + family + "' !important }");
+  }
+
+  load('https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function($) {
+    var $head = $("head");
+    var $body = $("body");
+    var $selector = $("<select/>")
+                    .attr('id', 'font-tester-selector')
+                    .change(selectorChange);
+    var $toolbar = $('<div/>')
+                    .attr('id=', 'font-tester-toolbar')
+                    .append($selector);
+
+    $head.append("<style>#font-tester-toolbar { border-bottom: 1px solid #aaa; position: fixed; top: 0; width: 100%; height: 25px; z-index: 10000; text-align: left; background: gainsboro; }</style>");
+
+    $body.append($div);
+
+    $.getJSON(API_PATH, function(data) {
+      $selector.data('font-data', data);
+      loadFonts($selector);
+    })
+  });
+})(document)
